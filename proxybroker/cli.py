6b8783560ebe80eeb3adcd127455ea8191bf189b
bendzaminas <39025147+bendzaminas@users.noreply.github.com>
2021-01-23 20:19:16 +0200

    Update cli.py

--- proxybroker/cli.py
+++ proxybroker/cli.py
@@ -406,6 +406,10 @@ def cli(args=sys.argv[1:]):
                 limit=ns.limit,
             )
         )
+    if ns.command == 'stats':
+        tasks.append(
+            broker.show_stats()
+        )
     elif ns.command == 'grab':
         tasks.append(broker.grab(countries=ns.countries, limit=ns.limit))
     elif ns.command == 'serve':
